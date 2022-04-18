7ec2de091675671e1654f4f8baf60d37b2221508
José Alfredo Pedrozo <j.alfredopedrozo@gmail.com>
2022-04-18 13:14:33 +0000

    Feature aulas 1 e 2

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// Aula 1 Exercicios:
+console.log("Aula 1 Exercicios");
+
+var nome = "José",
+  idade = "19 anos",
+  comidaFavorita = "Sushi";
+
+console.log(nome, idade, comidaFavorita);
+
+let teste1, teste2, teste3, teste4, teste5;
+console.log(teste1, teste2, teste3, teste4, teste5);
+
+console.log("________________________________");
+
+// Aula 2 Exercicios:
+console.log("Aula 2 Exercicios");
+
+var cidade = "Florianópolis";
+var mes = "6";
+var ano = 2000;
+
+var Nome = "José",
+  Sobrenome = "Pedrozo";
+console.log(Nome + " " + Sobrenome);
+
+let frase = "It's time";
+console.log(frase);
+
+console.log(typeof Nome);
+
+console.log("________________________________");
